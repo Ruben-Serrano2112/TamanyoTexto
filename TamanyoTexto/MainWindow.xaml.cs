1f5f7996139c15b068584d16abc320c9017dd64c
Ruben <ruben.serrano@iesdoctorbalmis.com>
2021-10-01 11:52:55 +0200

    Agregar archivos de proyecto.

---
+++ TamanyoTexto/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+
+namespace TamanyoTexto
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TamañoTexto_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (sender == ButtonPequeño)
+            {
+                PruebaTextblock.FontSize = 36;
+            }
+            if (sender == ButtonMediano)
+            {
+                PruebaTextblock.FontSize = 48;
+            }
+            if(sender == ButtonGrande)
+            {
+                PruebaTextblock.FontSize = 72;
+            }
+        }
+    }
+}
